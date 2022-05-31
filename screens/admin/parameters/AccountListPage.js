b208b07c28c7ca8ad893ea7bd6a1a5b7ed08d8af
cyril cabrolier <cyr.cab@hotmail.fr>
2022-05-31 16:27:03 +0200

    adding list component

--- screens/admin/parameters/AccountListPage.js
+++ screens/admin/parameters/AccountListPage.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
+import axios from '../../helpers/axios.config';
+
+import ListElems from '../../../components/ListElems';
 
 const AccountListPage = () => {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/users')
+      .then((response) => response.data)
+      .then((data) => setUserList(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Main>
       <Title>Ceci est la page de paramètres des comptes</Title>
+      <ListElems elems={userList} />
     </Main>
   );
 };
