7f88def38d226e65db52c305bd8e8f89b324aa7a
cyril cabrolier <cyr.cab@hotmail.fr>
2022-08-16 16:53:42 +0200

    api adress changed

--- helpers/api/axios.config.js
+++ helpers/api/axios.config.js
@@ -2,9 +2,8 @@ import axiosDefault from 'axios';
 import { API_URL } from '@env';
 
 const axios = axiosDefault.create({
-  baseURL: 'http://172.20.10.2:5001',
+  baseURL: API_URL,
   withCredentials: true,
-  // baseURL: 'http://192.168.1.16:5001',
 });
 
 export default axios;
